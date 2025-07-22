58561d76d7a8e04cd58db1bf1f78e0138047ffdf
GrzegorzZajac000 <grzegorz.zajac000@gmail.com>
2025-07-22 17:53:13 +0200

    $1973 InstallPage - Mobile apps QR added

--- entrypoints/install/components/DownloadMobileApp/default.jsx
+++ entrypoints/install/components/DownloadMobileApp/default.jsx
@@ -4,8 +4,12 @@
 // Licensed under the Business Source License 1.1
 // See LICENSE file for full terms
 
-import AppStore from '@/assets/install-page/appstore.png?url';
-import GooglePlay from '@/assets/install-page/googleplay.png?url';
+import S from '../../Install.module.scss';
+import AppStore from '@/assets/install-page/appstore.png?react';
+import GooglePlay from '@/assets/install-page/googleplay.png?react';
+import ScanIcon from '@/assets/install-page/scan.svg?react';
+import PassIosQR from '@/assets/install-page/pass-ios-qr.svg?react';
+import PassAndroidQR from '@/assets/install-page/pass-android-qr.svg?react';
 
 /** 
 * Function component for the DownloadMobileAppDefault.
@@ -14,12 +18,56 @@ import GooglePlay from '@/assets/install-page/googleplay.png?url';
 const DownloadMobileAppDefault = () => {
   return (
     <>
-      <a href="https://apps.apple.com/us/app/2fas-pass/id6504464955" target="_blank" rel="noopener noreferrer">
-        <img src={AppStore} alt="App Store" loading="lazy" />
-      </a>
-      <a href="https://play.google.com/store/apps/details?id=com.twofasapp.pass" target="_blank" rel="noopener noreferrer">
-        <img src={GooglePlay} alt="Google Play" loading="lazy" />
-      </a>
+      <div className={S.appsDownload}>
+        <a
+          href="https://apps.apple.com/us/app/2fas-pass/id6504464955"
+          target="_blank"
+          rel="noopener noreferrer"
+          title={browser.i18n.getMessage('install_download_ios_app')}
+        >
+          <img
+            className={S.appStore}
+            src={AppStore}
+            alt="App Store"
+            loading="lazy"
+          />
+        </a>
+        <button className={S.appsDownloadButton} type='button'>
+          <span className={S.appsDownloadButtonContent}>
+            <ScanIcon />
+            <span>{browser.i18n.getMessage('install_scan')}</span>
+          </span>
+          <span className={S.appsDownloadButtonTooltip}>
+            <span>{browser.i18n.getMessage('install_point_camera')}</span>
+            <PassIosQR />
+          </span>
+        </button>
+      </div>
+      <div className={S.appsDownload}>
+        <a
+          href="https://play.google.com/store/apps/details?id=com.twofasapp.pass"
+          target="_blank"
+          rel="noopener noreferrer"
+          title={browser.i18n.getMessage('install_download_android_app')}
+        >
+          <img
+            className={S.googlePlay}
+            src={GooglePlay}
+            alt="Google Play"
+            loading="lazy"
+          />
+        </a>
+        <button className={S.appsDownloadButton} type='button'>
+          <span className={S.appsDownloadButtonContent}>
+            <ScanIcon />
+            <span>{browser.i18n.getMessage('install_scan')}</span>
+          </span>
+          <span className={S.appsDownloadButtonTooltip}>
+            <span>{browser.i18n.getMessage('install_point_camera')}</span>
+            <PassAndroidQR />
+          </span>
+        </button>
+      </div>
     </>
   );
 };
