c870f6f8e5a069d761cefec454129d1ff99c80fb
GrzegorzZajac000 <grzegorz.zajac000@gmail.com>
2025-07-24 14:31:31 +0200

    $1973 contentScripts invalidation fix

--- entrypoints/content/index.js
+++ entrypoints/content/index.js
@@ -7,6 +7,7 @@
 import '@/partials/TwofasNotification/TwofasNotification.scss';
 import contentOnMessage from './events/contentOnMessage';
 import isCryptoAvailable from '@/partials/functions/isCryptoAvailable';
+import ifCtxIsInvalid from '@/partials/contentScript/ifCtxIsInvalid';
 
 export default defineContentScript({
   matches: ['https://*/*', 'http://*/*'],
@@ -20,7 +21,13 @@ export default defineContentScript({
       const emptyFunc = () => {};
       const cryptoAvailable = isCryptoAvailable();
 
-      if (ctx.isTopFrame) {
+      const removeListeners = () => {
+        browser.runtime.onMessage.removeListener(handleMessage);
+        window.removeEventListener('error', emptyFunc);
+        window.removeEventListener('unhandledrejection', emptyFunc);
+      };
+
+      if (ctx?.isTopFrame && ctx?.isValid) {
         const ui = await createShadowRootUi(ctx, {
           position: 'relative',
           mode: 'closed',
@@ -31,27 +38,32 @@ export default defineContentScript({
             shadow.children[0].setAttribute('style', 'pointer-events: none !important;');
             shadow.children[0].getElementsByTagName('body')[0].style = 'margin: 0 !important; padding: 0 !important; overflow: hidden !important;';
 
-            handleMessage = (request, sender, sendResponse) => contentOnMessage(request, sender, sendResponse, ctx.isTopFrame, container, cryptoAvailable);
+            handleMessage = (request, sender, sendResponse) => {
+              if (ifCtxIsInvalid(ctx, removeListeners)) {
+                return;
+              }
+
+              return contentOnMessage(request, sender, sendResponse, ctx.isTopFrame, container, cryptoAvailable);
+            };
             browser.runtime.onMessage.addListener(handleMessage);
           }
         });
 
         ui.mount();
       } else {
-        handleMessage = (request, sender, sendResponse) => contentOnMessage(request, sender, sendResponse, ctx.isTopFrame, null, cryptoAvailable);
+        handleMessage = (request, sender, sendResponse) => {
+          if (ifCtxIsInvalid(ctx, removeListeners)) {
+            return;
+          }
+
+          return contentOnMessage(request, sender, sendResponse, ctx.isTopFrame, null, cryptoAvailable);
+        };
         browser.runtime.onMessage.addListener(handleMessage);
       }
 
       window.addEventListener('error', emptyFunc);
       window.addEventListener('unhandledrejection', emptyFunc);
-
-      const removeListeners = () => {
-        browser.runtime.onMessage.removeListener(handleMessage);
-        window.removeEventListener('error', emptyFunc);
-        window.removeEventListener('unhandledrejection', emptyFunc);
-      };
-
-      ctx.onInvalidated(removeListeners);
+      
       window.addEventListener('beforeunload', removeListeners, { once: true });
     } catch (e) {
       handleError(e);
--- entrypoints/focus.content.js
+++ entrypoints/focus.content.js
@@ -6,24 +6,45 @@
 
 import isCryptoAvailable from '@/partials/functions/isCryptoAvailable';
 import focusOnMessage from './focus/events/focusOnMessage';
+import focusFunc from './focus/functions/focusFunc';
+import ifCtxIsInvalid from '@/partials/contentScript/ifCtxIsInvalid';
 
-// FUTURE - remove listeners
 export default defineContentScript({
   matches: ['https://*/*', 'http://*/*'],
   all_frames: true,
   match_about_blank: true,
   registration: 'manifest',
-  async main () {
+  async main (ctx) {
+    const emptyFunc = () => {};
     const cryptoAvailable = isCryptoAvailable();
+    const focusFuncAction = () => {
+      if (ifCtxIsInvalid(ctx, removeListeners)) {
+        return;
+      }
 
-    browser.runtime.onMessage.addListener(focusOnMessage);
+      return focusFunc(cryptoAvailable);
+    };
 
-    window.addEventListener('focus', async () => {
-      await browser.runtime.sendMessage({
-        action: REQUEST_ACTIONS.TAB_FOCUS,
-        target: REQUEST_TARGETS.BACKGROUND,
-        cryptoAvailable
-      });
-    });
+    const focusOnMessageHandler = (request, sender, sendResponse) => {
+      if (ifCtxIsInvalid(ctx, removeListeners)) {
+        return;
+      }
+
+      return focusOnMessage(request, sender, sendResponse);
+    };
+
+    browser.runtime.onMessage.addListener(focusOnMessageHandler);
+    window.addEventListener('focus', focusFuncAction);
+    window.addEventListener('error', emptyFunc);
+    window.addEventListener('unhandledrejection', emptyFunc);
+
+    const removeListeners = () => {
+      browser.runtime.onMessage.removeListener(focusOnMessageHandler);
+      window.removeEventListener('focus', focusFuncAction);
+      window.removeEventListener('error', emptyFunc);
+      window.removeEventListener('unhandledrejection', emptyFunc);
+    };
+
+    window.addEventListener('beforeunload', removeListeners, { once: true });
   }
 });
