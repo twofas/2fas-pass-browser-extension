6ca2ad3a39c6ebeb47d6369596d3a47fa6770bc9
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-09-22 13:07:17 +0200

    $2014 Models - Login, SecureNote, Tag

---
+++ utils/models/SecureNote.js
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright © 2025 Two Factor Authentication Service, Inc.
+// Licensed under the Business Source License 1.1
+// See LICENSE file for full terms
+
+class SecureNote {
+  contentType = 'secureNote';
+  contentVersion = 1;
+
+  constructor (data) {
+    this.id = data.id;
+    this.name = data.content.name;
+    this.s_text = data.content.s_text;
+    this.securityType = data.content.securityType || 0; // Check default
+    this.tags = data.tags || [];
+    this.createdAt = data.createdAt || null;
+    this.updatedAt = data.updatedAt || null;
+  }
+}
+
+export default SecureNote;
---
+++ utils/models/Tag.js
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright © 2025 Two Factor Authentication Service, Inc.
+// Licensed under the Business Source License 1.1
+// See LICENSE file for full terms
+
+class Tag {
+  constructor (data) {
+    this.id = data.id;
+    this.name = data.content.name;
+    this.color = data?.content?.color || null;
+    this.position = data?.content?.position || null;
+    this.updatedAt = data.updatedAt || null;
+  }
+}
+
+export default Tag;
