6dea8344a6da6cd0b88c0486dc7bb924934c95d4
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-09-22 13:45:16 +0200

    $2014 deviceUpdate - scheme added

--- partials/WebSocket/utils/deviceUpdate.js
+++ partials/WebSocket/utils/deviceUpdate.js
@@ -8,10 +8,10 @@
 * Updates the device with the given UUID.
 * @async
 * @param {string} uuid - The UUID of the device to update.
-* @param {object} payload - The payload to update the device with.
+* @param {object} json - The JSON object containing the device data.
 * @return {string} The ID of the updated device.
 */
-const deviceUpdate = async (uuid, payload) => {
+const deviceUpdate = async (uuid, json) => {
   let devices = await storage.getItem('local:devices') || [];
   const device = devices.find(d => d.uuid === uuid);
   
@@ -19,13 +19,14 @@ const deviceUpdate = async (uuid, payload) => {
     throw new TwoFasError(TwoFasError.internalErrors.deviceNotFound, { additional: { func: 'deviceUpdate' } });
   }
   
-  device.name = payload.deviceName || '2FAS Pass Mobile App';
-  device.id = payload.deviceId;
-  device.platform = payload.deviceOs || '';
-  device.supportedFeatures = payload.supportedFeatures || [];
+  device.name = json?.payload?.deviceName || '2FAS Pass Mobile App';
+  device.id = json?.payload?.deviceId;
+  device.scheme = json?.scheme || null;
+  device.platform = json?.payload?.deviceOs || '';
+  device.supportedFeatures = json?.payload?.supportedFeatures || [];
   device.updatedAt = Date.now();
 
-  devices = devices.filter(d => d?.id === payload.deviceId && d?.uuid === uuid); // Remove old devices with the same ID
+  devices = devices.filter(d => d?.id === json?.payload?.deviceId && d?.uuid === uuid); // Remove old devices with the same ID
 
   await storage.setItem('local:devices', devices);
 
