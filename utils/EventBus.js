16ab737ad76ecbd737629137543e4e4f20bb8801
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-09-22 15:13:42 +0200

    $2014 EventBus

---
+++ utils/EventBus.js
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright © 2025 Two Factor Authentication Service, Inc.
+// Licensed under the Business Source License 1.1
+// See LICENSE file for full terms
+
+/**
+* A simple event bus for managing events across the application.
+*/
+class EventBus {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event, callback) {
+    if (this.listeners[event]) {
+      this.listeners[event] = this.listeners[event].filter(
+        (listener) => listener !== callback
+      );
+    }
+  }
+
+  emit(event, data) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach((listener) => listener(data));
+    }
+  }
+}
+
+const eventBus = new EventBus();
+
+export { EventBus };
+export default eventBus;
