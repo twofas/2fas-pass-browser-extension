b50110a69067629c49a73cbf3680b24bb135a562
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-09-24 12:22:13 +0200

    $2014 FetchState const

---
+++ entrypoints/popup/routes/Fetch/constants/index.js
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright © 2025 Two Factor Authentication Service, Inc.
+// Licensed under the Business Source License 1.1
+// See LICENSE file for full terms
+
+export { default as FETCH_STATE } from './FETCH_STATE';
+export { default as PULL_REQUEST_STATUSES } from './PULL_REQUEST_STATUSES';
+export { default as PULL_REQUEST_TYPES } from './PULL_REQUEST_TYPES';
