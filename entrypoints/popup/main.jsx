20e84feddd4a3c85a3d261fa460feea6ca37e71e
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-09-25 19:36:24 +0200

    $2014 Routing optimizations; auth hook optimizations

--- entrypoints/popup/main.jsx
+++ entrypoints/popup/main.jsx
@@ -11,10 +11,5 @@ import '@/partials/global-styles/selects.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Popup from './Popup.jsx';
-import { HashRouter } from 'react-router';
 
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <Popup />
-  </HashRouter>
-);
+createRoot(document.getElementById('root')).render(<Popup />);
