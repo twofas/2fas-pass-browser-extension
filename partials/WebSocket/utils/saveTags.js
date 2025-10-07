c9ed77d7768da02fc6ce4fdf66e5947cf62873f1
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-10-07 15:09:47 +0200

    $2014 saveTags update + validation

--- partials/WebSocket/utils/saveTags.js
+++ partials/WebSocket/utils/saveTags.js
@@ -5,15 +5,37 @@
 // See LICENSE file for full terms
 
 import getKey from '@/partials/sessionStorage/getKey';
+import decompress from '@/partials/gzip/decompress';
+import compress from '@/partials/gzip/compress';
+import Tag from '@/partials/models/Tag';
 
 /** 
 * Saves the tags in the session storage.
 * @async
-* @param {string} gzipData - The gzip data to be saved.
+* @param {string} gzipInputData - The gzip input data to be saved.
 * @param {string} deviceId - The device ID.
 * @return {boolean} Returns true if the tags are saved successfully, otherwise false.
 */
-const saveTags = async (gzipData, deviceId) => {
+const saveTags = async (gzipInputData, deviceId) => {
+  // @TODO: Add ifs for empty data etc.
+  const decompressedData = await decompress(Base64ToArrayBuffer(gzipInputData));
+  const decompressedJSON = JSON.parse(decompressedData);
+
+  const validTags = [];
+
+  for (const tagData of decompressedJSON) {
+    try {
+      const tag = new Tag(tagData);
+      validTags.push(tag);
+    } catch {}
+  }
+
+  console.log('Valid tags:', validTags);
+
+  const jsonString = JSON.stringify(validTags);
+  const gzipDataAB = await compress(jsonString);
+  const gzipData = ArrayBufferToBase64(gzipDataAB);
+
   const tagsKey = await getKey('tags', { deviceId });
   await storage.setItem(`session:${tagsKey}`, gzipData);
 
