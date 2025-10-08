ab4c04cf59ce6888a1fd0f811636d4126e3cfe28
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-10-08 09:22:28 +0200

    $2014 SecureNote model improvement

--- partials/models/SecureNote.js
+++ partials/models/SecureNote.js
@@ -8,16 +8,28 @@ class SecureNote {
   static contentType = 'secureNote';
   static contentVersion = 1;
 
-  contentVersion = 1;
+  constructor (secureNoteData) {
+    validate(secureNoteData && typeof secureNoteData === 'object', 'Invalid secure note data');
 
-  constructor (data) {
-    this.id = data.id;
-    this.name = data.content.name;
-    this.s_text = data.content.s_text;
-    this.securityType = data.content.securityType || 0; // Check default
-    this.tags = data.tags || [];
-    this.createdAt = data.createdAt || null;
-    this.updatedAt = data.updatedAt || null;
+    validate(isValidUUID(secureNoteData.id), 'Invalid or missing id: must be a valid UUID');
+    validate(isValidInteger(secureNoteData.createdAt), 'Invalid or missing createdAt: must be an integer');
+    validate(isValidInteger(secureNoteData.updatedAt), 'Invalid or missing updatedAt: must be an integer');
+    validate(isValidInteger(secureNoteData.securityType, 0, 2), 'Invalid or missing securityType: must be an integer between 0 and 2');
+    validateOptional(secureNoteData.tags, tags => isValidArray(tags, tag => isValidString(tag)), 'Invalid tags: must be an array of strings');
+
+    const content = secureNoteData?.content ? JSON.parse(secureNoteData.content) : {};
+
+    validate(content && typeof content === 'object', 'Invalid secureNote content data');
+    validateOptional(content.name, isValidString, 'Invalid content.name: must be a string');
+    validateOptional(content.s_text, isValidBase64, 'Invalid content.s_text: must be a base64 string');
+
+    this.id = secureNoteData.id;
+    this.name = content.name;
+    this.s_text = content.s_text;
+    this.securityType = secureNoteData.securityType;
+    this.tags = secureNoteData.tags || [];
+    this.createdAt = secureNoteData.createdAt || null;
+    this.updatedAt = secureNoteData.updatedAt || null;
   }
 }
 
