63b25d798e8b1e6e5c703b67ec940cc1717d8735
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-10-14 10:54:34 +0200

    $2014 REQUEST_STRING_ACTIONS const

---
+++ constants/REQUEST_STRING_ACTIONS.js
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright © 2025 Two Factor Authentication Service, Inc.
+// Licensed under the Business Source License 1.1
+// See LICENSE file for full terms
+
+const REQUEST_STRING_ACTIONS = {
+  SET: 'set',
+  GENERATE: 'generate'
+};
+
+Object.freeze(REQUEST_STRING_ACTIONS);
+
+export default REQUEST_STRING_ACTIONS;
