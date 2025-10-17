ffb645e4e4e5173138b0f4fbb9d0b7e4856ca260
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-10-17 17:21:00 +0200

    $2014 updatedData v2 - WIP

--- partials/WebSocket/fetch/updateData/updated.js
+++ partials/WebSocket/fetch/updateData/updated.js
@@ -12,6 +12,7 @@ import getKey from '@/partials/sessionStorage/getKey';
 import compressObject from '@/partials/gzip/compressObject';
 import saveItems from '@/partials/WebSocket/utils/saveItems';
 import { ENCRYPTION_KEYS } from '@/constants';
+import Login from '@/partials/models/Login';
 
 /** 
 * Handles the update of a item after it has been modified.
@@ -22,73 +23,72 @@ import { ENCRYPTION_KEYS } from '@/constants';
 * @param {string} messageId - The message ID.
 * @return {Promise<Object>} Object containing returnUrl and returnToast.
 */
-const updateDataUpdated = async (data, state, hkdfSaltAB, sessionKeyForHKDF, messageId) => {
+const updateDataUpdated = async (info, state, hkdfSaltAB, sessionKeyForHKDF, messageId) => {
   // @TODO: Change to v2!
+  console.log(info, state.data);
 
-  if (!data || !data?.login || !data?.login?.deviceId || !data?.login?.id) {
+  if (!info || !info?.data) { // @TODO: Fix in future
     throw new TwoFasError(TwoFasError.errors.pullRequestActionUpdateLoginUpdatedWrongData);
   }
 
   try {
     const [items, itemsKeys] = await Promise.all([
       getItems(),
-      getItemsKeys(state.data.deviceId)
+      getItemsKeys(state.data.vaultId, state.data.deviceId)
     ]);
 
     // Update login & clear alarm if exists
     const itemIndex = items.findIndex(item => item.id === state.data.itemId);
-    let item = items.find(item => item.id === state.data.itemId);
+    const originalItem = items.find(item => item.id === state.data.itemId);
 
-    if (item && item.securityType === SECURITY_TIER.HIGHLY_SECRET) {
+    if (originalItem && originalItem.securityType === SECURITY_TIER.HIGHLY_SECRET) {
       await browser.alarms.clear(`sifT2Reset-${state.data.itemId}`);
     }
 
-    item = data.login;
+    const item = new Login(info.data, state.data.vaultId, state.data.deviceId);
+    console.log('ITEM:', item);
 
-    if (data.login.securityType === SECURITY_TIER.SECRET) {
-      item.internalData.type = 'added';
-    }
+    // if (data.login.securityType === SECURITY_TIER.SECRET) {
+    //   item.internalData.type = 'added';
+    // }
 
-    items[itemIndex] = item;
+    // items[itemIndex] = item;
 
-    // Compress items
-    const itemsGZIP = await compressObject(items);
+    // if (data.login.securityType === SECURITY_TIER.SECRET) {
+    //   // generate encryptionItemT3Key
+    //   const encryptionItemT3Key = await generateEncryptionAESKey(hkdfSaltAB, ENCRYPTION_KEYS.ITEM_T3.crypto, sessionKeyForHKDF, true);
+    //   const encryptionItemT3KeyAESRaw = await window.crypto.subtle.exportKey('raw', encryptionItemT3Key);
+    //   const encryptionItemT3KeyAES_B64 = ArrayBufferToBase64(encryptionItemT3KeyAESRaw);
 
-    if (data.login.securityType === SECURITY_TIER.SECRET) {
-      // generate encryptionItemT3Key
-      const encryptionItemT3Key = await generateEncryptionAESKey(hkdfSaltAB, ENCRYPTION_KEYS.ITEM_T3.crypto, sessionKeyForHKDF, true);
-      const encryptionItemT3KeyAESRaw = await window.crypto.subtle.exportKey('raw', encryptionItemT3Key);
-      const encryptionItemT3KeyAES_B64 = ArrayBufferToBase64(encryptionItemT3KeyAESRaw);
+    //   // save encryptionItemT3Key in session storage
+    //   const itemT3Key = await getKey(ENCRYPTION_KEYS.ITEM_T3_NEW.sK, { deviceId: data.login.deviceId, itemId: data.login.id });
+    //   await storage.setItem(`session:${itemT3Key}`, encryptionItemT3KeyAES_B64);
+    // } else if (data.login.securityType === SECURITY_TIER.HIGHLY_SECRET) {
+    //   // generate encryptionItemT2Key
+    //   const encryptionItemT2Key = await generateEncryptionAESKey(hkdfSaltAB, ENCRYPTION_KEYS.ITEM_T2.crypto, sessionKeyForHKDF, true);
+    //   const encryptionItemT2KeyAESRaw = await window.crypto.subtle.exportKey('raw', encryptionItemT2Key);
+    //   const encryptionItemT2KeyAES_B64 = ArrayBufferToBase64(encryptionItemT2KeyAESRaw);
 
-      // save encryptionItemT3Key in session storage
-      const itemT3Key = await getKey(ENCRYPTION_KEYS.ITEM_T3_NEW.sK, { deviceId: data.login.deviceId, itemId: data.login.id });
-      await storage.setItem(`session:${itemT3Key}`, encryptionItemT3KeyAES_B64);
-    } else if (data.login.securityType === SECURITY_TIER.HIGHLY_SECRET) {
-      // generate encryptionItemT2Key
-      const encryptionItemT2Key = await generateEncryptionAESKey(hkdfSaltAB, ENCRYPTION_KEYS.ITEM_T2.crypto, sessionKeyForHKDF, true);
-      const encryptionItemT2KeyAESRaw = await window.crypto.subtle.exportKey('raw', encryptionItemT2Key);
-      const encryptionItemT2KeyAES_B64 = ArrayBufferToBase64(encryptionItemT2KeyAESRaw);
+    //   // save encryptionItemT2Key in session storage
+    //   const itemT2Key = await getKey(ENCRYPTION_KEYS.ITEM_T2.sK, { deviceId: data.login.deviceId, itemId: data.login.id });
+    //   await storage.setItem(`session:${itemT2Key}`, encryptionItemT2KeyAES_B64);
+    // } else {
+    //   throw new TwoFasError(TwoFasError.errors.pullRequestActionUpdateLoginUpdatedWrongSecurityType);
+    // }
 
-      // save encryptionItemT2Key in session storage
-      const itemT2Key = await getKey(ENCRYPTION_KEYS.ITEM_T2.sK, { deviceId: data.login.deviceId, itemId: data.login.id });
-      await storage.setItem(`session:${itemT2Key}`, encryptionItemT2KeyAES_B64);
-    } else {
-      throw new TwoFasError(TwoFasError.errors.pullRequestActionUpdateLoginUpdatedWrongSecurityType);
-    }
+    // // Remove items from session storage (by itemsKeys)
+    // await storage.removeItems(itemsKeys);
 
-    // Remove items from session storage (by itemsKeys)
-    await storage.removeItems(itemsKeys);
+    // // saveItems
+    // await saveItems(itemsGZIP, state.data.vaultId, state.data.deviceId);
 
-    // saveItems
-    // await saveItems(itemsGZIP, data.login.deviceId);
+    // // Set alarm for reset T2 SIF
+    // if (data.login.securityType === SECURITY_TIER.HIGHLY_SECRET) {
+    //   const sifResetTime = data.expireInSeconds && data.expireInSeconds > 30 ? data.expireInSeconds * 60 : config.passwordResetDelay;
+    //   await browser.alarms.create(`sifT2Reset-${data.login.id}`, { delayInMinutes: sifResetTime });
+    // }
 
-    // Set alarm for reset T2 SIF
-    if (data.login.securityType === SECURITY_TIER.HIGHLY_SECRET) {
-      const sifResetTime = data.expireInSeconds && data.expireInSeconds > 30 ? data.expireInSeconds * 60 : config.passwordResetDelay;
-      await browser.alarms.create(`sifT2Reset-${data.login.id}`, { delayInMinutes: sifResetTime });
-    }
-
-    await sendPullRequestCompleted(messageId);
+    // await sendPullRequestCompleted(messageId);
 
     return {
       returnUrl: `/details/${state.data.itemId}`,
