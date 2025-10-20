30343b4fe6928310c6aa71f607064de639581130
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-10-20 08:42:35 +0200

    $2014 FullSync - cancel action

--- partials/WebSocket/fetch/fullSync/cancel.js
+++ partials/WebSocket/fetch/fullSync/cancel.js
@@ -4,10 +4,28 @@
 // Licensed under the Business Source License 1.1
 // See LICENSE file for full terms
 
-const fullSyncCancel = async () => {
-  return {
-    returnUrl: '/'
-  };
+import sendPullRequestCompleted from '../sendPullRequestCompleted';
+
+/** 
+* Function to handle the full sync cancellation.
+* @param {string} messageId - The ID of the message to cancel.
+* @return {Promise<Object>} The result of the cancellation.
+*/
+
+const fullSyncCancel = async (messageId) => {
+  try {
+    await sendPullRequestCompleted(messageId);
+
+    return {
+      returnUrl: '/',
+      returnToast: {
+        text: browser.i18n.getMessage('fetch_full_sync_cancel_toast'),
+        type: 'info'
+      }
+    };
+  } catch (e) {
+    throw new TwoFasError(TwoFasError.errors.pullRequestActionDeleteCancelError, { event: e });
+  }
 };
 
 export default fullSyncCancel;
