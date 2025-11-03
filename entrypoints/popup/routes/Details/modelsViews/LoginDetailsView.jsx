2499c82d7b32a4545eeab39f69727625732b064f
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-11-03 09:48:33 +0100

    $2014 Details - Password update fix

--- entrypoints/popup/routes/Details/modelsViews/LoginDetailsView.jsx
+++ entrypoints/popup/routes/Details/modelsViews/LoginDetailsView.jsx
@@ -98,7 +98,7 @@ function LoginDetailsView () {
       if (data.passwordMobile) {
         stateData.content.s_password = { value: '', action: REQUEST_STRING_ACTIONS.GENERATE };
       } else {
-        stateData.content.s_password = { value: e?.editedPassword ? valueToNFKD(e.editedPassword) : '', action: REQUEST_STRING_ACTIONS.SET };
+        stateData.content.s_password = { value: data?.item?.internalData?.editedPassword ? valueToNFKD(data.item.internalData.editedPassword) : '', action: REQUEST_STRING_ACTIONS.SET };
       }
     }
 
