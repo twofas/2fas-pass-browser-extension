df0380d8c7f7efe2cd2b18cf502f4e0c57c370e2
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-11-13 15:28:32 +0100

    $2029 SecureNoteItemView

---
+++ entrypoints/popup/routes/ThisTab/components/Item/components/CopyNameBtn.jsx
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+//
+// Copyright © 2025 Two Factor Authentication Service, Inc.
+// Licensed under the Business Source License 1.1
+// See LICENSE file for full terms
+
+import S from '../../../ThisTab.module.scss';
+import { lazy } from 'react';
+import handleName from '../../../functions/serviceList/handleName';
+
+const CopyNameIcon = lazy(() => import('@/assets/popup-window/copy-name.svg?react'));
+
+const CopyNameBtn = ({ item }) => {
+  return (
+    <button
+      onClick={async () => await handleName(item.deviceId, item.vaultId, item.id)}
+      title={browser.i18n.getMessage('this_tab_copy_password')}
+    >
+      <CopyNameIcon className={S.serviceName} />
+    </button>
+  );
+};
+
+export default CopyNameBtn;
