d9159b4bd82c18f987866614f8f5711561a2b21f
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-11-13 16:38:49 +0100

    $2029 vitest fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,8 @@ import { defineConfig } from 'vitest/config';
 import { WxtVitest } from 'wxt/testing';
 
 export default defineConfig({
-  plugins: [WxtVitest()],
+  test: {
+    setupFiles: ['./vitest.setup.ts']
+  },
+  plugins: [WxtVitest()]
 });
