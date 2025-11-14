163092bd806860bd152b28cef7c850345e2716f8
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-11-14 14:57:01 +0100

    $2029 SecureNoteDetails; Code refactor

--- entrypoints/popup/routes/Details/components/SecureNoteText.jsx
+++ entrypoints/popup/routes/Details/components/SecureNoteText.jsx
@@ -4,28 +4,20 @@
 // Licensed under the Business Source License 1.1
 // See LICENSE file for full terms
 
+import S from '../Details.module.scss';
 import pI from '@/partials/global-styles/pass-input.module.scss';
 import bS from '@/partials/global-styles/buttons.module.scss';
 import { Field } from 'react-final-form';
-import { lazy } from 'react';
-import { LazyMotion } from 'motion/react';
-import * as m from 'motion/react-m';
-import { Link } from 'react-router';
-import { copyValue } from '@/partials/functions';
-import { findPasswordChangeUrl } from '../functions/checkPasswordChangeSupport';
-import { useState, useEffect, useRef } from 'react';
+import { lazy, useState } from 'react';
+import { LazyMotion, animate } from 'motion/react';
+import { useEffect, useRef } from 'react';
 import usePopupStateStore from '../../../store/popupState';
 
 const loadDomAnimation = () => import('@/features/domAnimation.js').then(res => res.default);
-const VisibleIcon = lazy(() => import('@/assets/popup-window/visible.svg?react'));
 const InfoIcon = lazy(() => import('@/assets/popup-window/info.svg?react'));
-const CopyIcon = lazy(() => import('@/assets/popup-window/copy-to-clipboard.svg?react'));
-const RefreshIcon = lazy(() => import('@/assets/popup-window/refresh.svg?react'));
-const ExternalLinkIcon = lazy(() => import('@/assets/popup-window/new-tab.svg?react'));
-const PasswordInput = lazy(() => import('@/entrypoints/popup/components/PasswordInput'));
 
  /**
-* Function to render the password input field.
+* Function to render the Secure Note text input field.
 * @param {Object} props - The component props.
 * @return {JSX.Element} The rendered component.
 */
@@ -36,14 +28,16 @@ function SecureNoteText (props) {
   const data = usePopupStateStore(state => state.data);
   const setData = usePopupStateStore(state => state.setData);
 
-  const previousPasswordValueRef = useRef(null);
+  const previousSifValueRef = useRef(null);
+  const textareaRef = useRef(null);
+  const [showTextarea, setShowTextarea] = useState(false);
 
   const getTextValue = () => {
     if (sifDecryptError) {
       return '';
     }
 
-    if (data.item.internalData.editedSif !== null) {
+    if (data.item.internalData.editedSif !== null && data.item.internalData.editedSif !== undefined) { // @TODO: isText?
       return data.item.internalData.editedSif;
     }
 
@@ -57,11 +51,70 @@ function SecureNoteText (props) {
   useEffect(() => {
     const currentTextValue = getTextValue();
 
-    if (currentTextValue !== previousPasswordValueRef.current) {
+    if (currentTextValue !== previousSifValueRef.current) {
       form.change('editedSif', currentTextValue);
-      previousPasswordValueRef.current = currentTextValue;
+      previousSifValueRef.current = currentTextValue;
     }
-  }, [data.item.internalData.editedSif, data.item.sifDecrypted, form]);
+  }, [data.item.internalData.editedSif, data.item.sifDecrypted, data.item.isSifDecrypted, sifDecryptError, form]);
+
+  useEffect(() => {
+    if (data?.revealSecureNote) {
+      setShowTextarea(true);
+
+      const animateExpand = async () => {
+        if (!textareaRef.current) {
+          return;
+        }
+
+        setTimeout(() => {
+          if (textareaRef.current) {
+            const scrollHeight = textareaRef.current.scrollHeight;
+
+            textareaRef.current.style.height = '19px';
+            textareaRef.current.style.overflowY = 'hidden';
+
+            animate(19, scrollHeight, {
+              duration: 0.3,
+              onUpdate: value => {
+                if (textareaRef.current) {
+                  textareaRef.current.style.height = `${value}px`;
+                }
+              },
+            }).then(() => {
+              if (textareaRef.current) {
+                textareaRef.current.style.overflowY = 'auto';
+              }
+            });
+          }
+        }, 0);
+      };
+
+      animateExpand();
+    } else if (!data?.revealSecureNote && showTextarea) {
+      const animateCollapse = async () => {
+        if (!textareaRef.current) {
+          return;
+        }
+
+        const currentHeight = textareaRef.current.offsetHeight;
+
+        textareaRef.current.style.overflowY = 'hidden';
+
+        await animate(currentHeight, 19, {
+          duration: 0.3,
+          onUpdate: value => {
+            if (textareaRef.current) {
+              textareaRef.current.style.height = `${value}px`;
+            }
+          },
+        });
+
+        setShowTextarea(false);
+      };
+
+      animateCollapse();
+    }
+  }, [data?.revealSecureNote, showTextarea]);
 
   const generateErrorOverlay = () => {
     if (!sifDecryptError) {
@@ -71,7 +124,7 @@ function SecureNoteText (props) {
     return (
       <div className={pI.passInputBottomOverlay}>
         <InfoIcon />
-        <span>{browser.i18n.getMessage('details_password_decrypt_error')}</span>
+        <span>{browser.i18n.getMessage('details_secure_note_decrypt_error')}</span>
       </div>
     );
   };
@@ -84,17 +137,13 @@ function SecureNoteText (props) {
     // FUTURE - move to separate component
     return (
       <div className={pI.passInputTooltip}>
-        <span>This information is only available on your mobile phone. Click the "Fetch" button at the top of this window to retrieve it.</span>
+        <span>{browser.i18n.getMessage('details_sif_not_fetched')}</span>
       </div>
     );
   };
 
-  // const handlePasswordOnMobileChange = () => {
-  //   setData('passwordMobile', !data?.passwordMobile);
-  // };
-
   const handleEditableClick = () => {
-    if (data?.passwordEditable) {
+    if (data?.sifEditable) {
       const itemData = data.item.toJSON();
       itemData.internalData = { ...data.item.internalData };
       const updatedItem = new (data.item.constructor)(itemData);
@@ -106,28 +155,15 @@ function SecureNoteText (props) {
       updatedItem.internalData.editedSif = null;
 
       setData('item', updatedItem);
-      setData('passwordEdited', false);
-      setData('passwordEditable', false);
+      setData('sifEdited', false);
+      setData('sifEditable', false);
+      setData('revealSecureNote', false);
       form.change('editedSif', data.item.isSifDecrypted ? data.item.sifDecrypted : '');
     } else {
-      setData('passwordEditable', true);
+      setData('sifEditable', true);
+      setData('revealSecureNote', true);
     }
   };
-
-  // const handlePasswordVisibleClick = () => {
-  //   setData('passwordVisible', !data?.passwordVisible);
-  // };
-  
-  // const handleChangePasswordClick = async e => {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-    
-  //   if (!changePasswordUrl) {
-  //     return;
-  //   }
-    
-  //   await browser.tabs.create({ url: changePasswordUrl });
-  // };
 
   const handleTextChange = e => {
     const newValue = e.target.value;
@@ -145,51 +181,61 @@ function SecureNoteText (props) {
     form.change('editedSif', newValue);
   };
 
+  const handleRevealToggle = () => {
+    setData('revealSecureNote', !data?.revealSecureNote);
+  };
+
   return (
     <LazyMotion features={loadDomAnimation}>
       <Field name='editedSif'>
         {() => (
-          <div className={`${pI.passInput} ${!data?.passwordEditable || data?.passwordMobile || sifDecryptError ? pI.disabled : ''} ${!originalItem?.isT3orT2WithSif ? pI.nonFetched : ''}`}>
+          <div className={`${pI.passInput} ${!data?.sifEditable || sifDecryptError ? pI.disabled : ''} ${!originalItem?.isT3orT2WithSif ? pI.nonFetched : ''}`}>
             <div className={pI.passInputTop}>
-              <label htmlFor='editedSif'>{browser.i18n.getMessage('password')}</label>
+              <label htmlFor='editedSif'>{browser.i18n.getMessage('secure_note')}</label>
               <button
                 type='button'
                 className={`${bS.btn} ${bS.btnClear} ${!originalItem?.isT3orT2WithSif || sifDecryptError ? bS.btnHidden : ''}`}
                 onClick={handleEditableClick}
               >
-                {data?.passwordEditable ? browser.i18n.getMessage('cancel') : browser.i18n.getMessage('edit')}
+                {data?.sifEditable ? browser.i18n.getMessage('cancel') : browser.i18n.getMessage('edit')}
               </button>
             </div>
             <div className={pI.passInputBottom}>
-              <textarea
-                value={getTextValue()}
-                // type={data?.passwordVisible ? 'text' : 'password'}
-                placeholder={!sifDecryptError && (!data?.passwordMobile && originalItem?.isT3orT2WithSif || data?.passwordEditable) ? browser.i18n.getMessage('placeholder_password') : ''}
-                id='editedSif'
-                onChange={handleTextChange}
-                // showPassword={data?.passwordVisible}
-                // isDecrypted={data.item.isSifDecrypted || data.item.internalData.editedSif !== null}
-                // state={!originalItem?.isT3orT2WithSif ? 'nonFetched' : ''}
-                // disabled={!data?.passwordEditable || data?.passwordMobile || sifDecryptError}
-                dir="ltr"
-                spellCheck="false"
-                autoCorrect="off"
-                autoComplete="off"
-                autoCapitalize="off"
-              />
-              {/* <div className={pI.passInputBottomButtons}>
-                <button
-                  type="button"
-                  onClick={handlePasswordVisibleClick}
-                  className={`${pI.iconButton} ${pI.visibleButton} ${!(originalItem?.isT3orT2WithSif || data?.passwordEditable) || sifDecryptError ? pI.hidden : ''}`}
-                  title={browser.i18n.getMessage('details_toggle_password_visibility')}
-                >
-                  <VisibleIcon />
-                </button>
-              </div> */}
+              {showTextarea ? (
+                <textarea
+                  ref={textareaRef}
+                  value={getTextValue()}
+                  placeholder={!sifDecryptError && (originalItem?.isT3orT2WithSif || data?.sifEditable) ? browser.i18n.getMessage('placeholder_secure_note') : ''}
+                  id='editedSif'
+                  className={S.detailsSecureNoteTextarea}
+                  disabled={!data?.sifEditable || sifDecryptError}
+                  onChange={handleTextChange}
+                  dir="ltr"
+                  spellCheck="false"
+                  autoCorrect="off"
+                  autoComplete="off"
+                  autoCapitalize="off"
+                />
+              ) : (
+                <input type='password' id='editedSif' value='***************************************' readOnly />
+              )}
 
               {generateSecurityTypeTooltip(originalItem)}
               {generateErrorOverlay()}
+            </div>
+            <div className={`${pI.passInputAdditional}`}>
+              <div className={`${bS.passToggle} ${bS.loaded}`}>
+                <input type="checkbox" name="reveal-secure-note" id="reveal-secure-note" checked={data?.revealSecureNote || false} onChange={handleRevealToggle} />
+                <label htmlFor="reveal-secure-note">
+                  <span className={bS.passToggleBox}>
+                    <span className={bS.passToggleBoxCircle}></span>
+                  </span>
+
+                  <span className={bS.passToggleText}>
+                    <span>{browser.i18n.getMessage('details_reveal_secure_note')}</span>
+                  </span>
+                </label>
+              </div>
             </div>
           </div>
         )}
