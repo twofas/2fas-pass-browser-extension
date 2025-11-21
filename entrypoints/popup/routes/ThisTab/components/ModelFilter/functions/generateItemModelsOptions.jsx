bd757c3e436feb5f7fe65b04c4700807f7cbaf51
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-11-21 15:25:30 +0100

    $2029 Icon lazy loading FIX

--- entrypoints/popup/routes/ThisTab/components/ModelFilter/functions/generateItemModelsOptions.jsx
+++ entrypoints/popup/routes/ThisTab/components/ModelFilter/functions/generateItemModelsOptions.jsx
@@ -6,9 +6,11 @@
 
 import S from '../../../ThisTab.module.scss';
 import { supportedFeatures } from '@/constants';
-import AllIcon from '@/assets/popup-window/items/all.svg?react';
-import LoginIcon from '@/assets/popup-window/items/login.svg?react';
-import SecureNoteIcon from '@/assets/popup-window/items/secure-note.svg?react';
+import { lazy } from 'react';
+
+const AllIcon = lazy(() => import('@/assets/popup-window/items/all.svg?react'));
+const LoginIcon = lazy(() => import('@/assets/popup-window/items/login.svg?react'));
+const SecureNoteIcon = lazy(() => import('@/assets/popup-window/items/secure-note.svg?react'));
 
 /** 
 * Function to generate item models options based on device supported features.
