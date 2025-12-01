870c0328573c4645b87dced814e45c33bd562ba5
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-12-01 16:33:10 +0100

    $2030 handleCardAutofill - WIP

--- entrypoints/popup/routes/ThisTab/functions/serviceList/handleCardAutofill.js
+++ entrypoints/popup/routes/ThisTab/functions/serviceList/handleCardAutofill.js
@@ -4,6 +4,19 @@
 // Licensed under the Business Source License 1.1
 // See LICENSE file for full terms
 
+import { sendMessageToAllFrames, sendMessageToTab, tabIsInternal, getLastActiveTab, popupIsInSeparateWindow, closeWindowIfNotInSeparateWindow, generateNonce } from '@/partials/functions';
+import injectCSIfNotAlready from '@/partials/contentScript/injectCSIfNotAlready';
+import { PULL_REQUEST_TYPES } from '@/constants';
+import PaymentCard from '@/partials/models/itemModels/PaymentCard';
+
+const showT2Toast = () => {
+  showToast(browser.i18n.getMessage('this_tab_can_t_autofill_t2'), 'info');
+};
+
+const showGenericToast = () => {
+  showToast(browser.i18n.getMessage('this_tab_can_t_autofill'), 'info');
+};
+
 /**
 * Handles the autofill action for PaymentCard items.
 * @async
@@ -12,7 +25,46 @@
 * @return {Promise<void>}
 */
 const handleCardAutofill = async (item, navigate) => {
-  showToast('Payment card autofill is not available yet', 'info');
+  const isHighlySecret = item.securityType === SECURITY_TIER.HIGHLY_SECRET;
+  const onTabError = isHighlySecret ? showT2Toast : showGenericToast;
+
+  let tab;
+
+  try {
+    tab = await getLastActiveTab(onTabError, t => !tabIsInternal(t));
+  } catch (e) {
+    await CatchError(e);
+  }
+
+  if (!tab) {
+    return;
+  }
+
+  try {
+    await injectCSIfNotAlready(tab.id, REQUEST_TARGETS.CONTENT);
+  } catch (e) {
+    onTabError();
+
+    if (!e.message.includes('showing error page')) {
+      await CatchError(e);
+    }
+
+    return;
+  }
+
+  const cryptoAvailableRes = await sendMessageToTab(tab.id, {
+    action: REQUEST_ACTIONS.GET_CRYPTO_AVAILABLE,
+    target: REQUEST_TARGETS.CONTENT
+  });
+
+  const hasCardData = item.sifExists;
+
+  // REQUEST_ACTIONS.AUTOFILL_CARD
+  // name, cardHolder, cardIssuer
+  // s_cardNumber, s_expirationDate, s_securityCode
+  // cardIssuer, expirationDateMonth, expirationDateYear can be option in select
+  // expirationDateMonth can be MM or M or month as text
+  // expirationDateYear can be YY or YYYY
 };
 
 export default handleCardAutofill;
