d3d5a97d1c7315f1630c48a73d6f6949368d4df9
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-12-02 16:52:44 +0100

    $2029 SecureNote - validations update

--- entrypoints/popup/routes/Details/components/AdditionalInfo.jsx
+++ entrypoints/popup/routes/Details/components/AdditionalInfo.jsx
@@ -34,6 +34,9 @@ function AdditionalInfo (props) {
   const { formData } = props;
   const { inputError } = formData;
 
+  const { formData } = props;
+  const { inputError } = formData;
+
   const handleAdditionalInfoEditable = async () => {
     if (data.additionalInfoEditable) {
       let item = await getItem(data.item.deviceId, data.item.vaultId, data.item.id);
