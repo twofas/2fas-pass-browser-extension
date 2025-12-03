780be90530a3db71124070978378958d929accb5
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-12-03 11:36:25 +0100

    $2030 CopyCardSecurityCodeBtn - fetch update

--- entrypoints/popup/routes/ThisTab/components/Item/components/CopyCardSecurityCodeBtn.jsx
+++ entrypoints/popup/routes/ThisTab/components/Item/components/CopyCardSecurityCodeBtn.jsx
@@ -6,13 +6,8 @@
 
 import S from '../../../ThisTab.module.scss';
 import handleCardSecurityCode from '../../../functions/serviceList/handleCardSecurityCode';
-import { Link } from 'react-router';
-import { useState, useRef, lazy, useLayoutEffect } from 'react';
-import getLoaderProgress from '@/partials/functions/getLoaderProgress';
-import { PULL_REQUEST_TYPES } from '@/constants';
-import PaymentCard from '@/partials/models/itemModels/PaymentCard';
+import { lazy } from 'react';
 
-const ServiceFetchIcon = lazy(() => import('@/assets/popup-window/service-fetch.svg?react'));
 const ServiceCopyIcon = lazy(() => import('@/assets/popup-window/card-security-code.svg?react'));
 
 /**
@@ -24,66 +19,6 @@ const ServiceCopyIcon = lazy(() => import('@/assets/popup-window/card-security-c
 * @return {JSX.Element} The rendered button element.
 */
 const CopyCardSecurityCodeBtn = ({ item, more, setMore }) => {
-  const [scheduledTime, setScheduledTime] = useState(false);
-  const loaderRef = useRef(null);
-  const intervalIdRef = useRef(0);
-
-  const getItemAlarm = async () => {
-    if (item?.securityType === SECURITY_TIER.HIGHLY_SECRET && item?.sifExists) {
-      let alarmTime;
-
-      try {
-        alarmTime = await browser.alarms.get(`sifT2Reset-${item.deviceId}|${item.vaultId}|${item.id}`);
-      } catch {
-        return false;
-      }
-
-      if (!alarmTime) {
-        return false;
-      }
-
-      setScheduledTime(alarmTime.scheduledTime);
-
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const updateProgress = () => {
-    if (!scheduledTime || !loaderRef.current) {
-      return;
-    }
-
-    const maxTime = item.internalData.sifResetTime ? item.internalData.sifResetTime * 60 * 1000 : config.passwordResetDelay * 60 * 1000;
-    const leftTime = maxTime - (scheduledTime - Date.now());
-    const percentTime = Math.round((leftTime / maxTime) * 100);
-    const progress = getLoaderProgress(100 - percentTime);
-
-    loaderRef.current.style.strokeDashoffset = progress;
-
-    if (percentTime >= 100) {
-      clearInterval(intervalIdRef.current);
-    }
-  };
-
-  useLayoutEffect(() => {
-    getItemAlarm()
-      .then(ok => {
-        if (ok) {
-          updateProgress();
-
-          intervalIdRef.current = setInterval(() => {
-            updateProgress();
-          }, 1000);
-        }
-      });
-
-    return () => {
-      clearInterval(intervalIdRef.current);
-    };
-  }, [item, scheduledTime]);
-
   if (item?.securityType === SECURITY_TIER.SECRET) {
     return (
       <button
@@ -98,41 +33,12 @@ const CopyCardSecurityCodeBtn = ({ item, more, setMore }) => {
       <button
         onClick={async () => await handleCardSecurityCode(item.deviceId, item.vaultId, item.id, more, setMore)}
         title={browser.i18n.getMessage('this_tab_copy_card_security_code')}
-        className={S.servicePasswordLoader}
       >
-        <svg
-          ref={loaderRef}
-          className={S.serviceLoader}
-          viewBox="0 0 96 96"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="48" cy="48" r="42" className={S.serviceLoaderBg} />
-          <circle cx="48" cy="48" r="42" />
-        </svg>
-
         <ServiceCopyIcon className={S.serviceCopy2} />
       </button>
     );
   } else {
-    return (
-      <Link
-        to='/fetch'
-        state={{
-          action: PULL_REQUEST_TYPES.SIF_REQUEST,
-          from: 'service',
-          data: {
-            itemId: item.id,
-            deviceId: item.deviceId,
-            vaultId: item.vaultId,
-            contentType: PaymentCard.contentType
-          }
-        }}
-        title={browser.i18n.getMessage('this_tab_fetch_card_security_code')}
-        prefetch='intent'
-      >
-        <ServiceFetchIcon className={S.serviceFetch} />
-      </Link>
-    );
+    return null;
   }
 };
 
