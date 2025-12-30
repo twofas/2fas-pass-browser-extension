30a3e2e8cce113e1890cc6e9d1ac39a4d7227e3f
Greg Zajac <grzegorz.zajac000@gmail.com>
2025-12-30 13:00:53 +0100

    $2030 PaymentCard - better card number & cvv masks

--- entrypoints/popup/components/PaymentCardSecurityCodeInput/getSecurityCodeMask.js
+++ entrypoints/popup/components/PaymentCardSecurityCodeInput/getSecurityCodeMask.js
@@ -4,6 +4,8 @@
 // Licensed under the Business Source License 1.1
 // See LICENSE file for full terms
 
+import { detectCardIssuer, CARD_ISSUER } from '../PaymentCardNumberInput/getCardNumberMask';
+
 const SECURITY_CODE_MASKS = {
   AMEX: {
     mask: '9999',
@@ -16,27 +18,43 @@ const SECURITY_CODE_MASKS = {
 };
 
 /**
+ * Returns maximum security code length for a given issuer.
+ * Matches iOS PaymentCardUtilityInteractor.maxSecurityCodeLength.
+ * @param {string|null} issuer - The card issuer identifier.
+ * @returns {number} Maximum security code length.
+ */
+const maxSecurityCodeLength = issuer => {
+  switch (issuer) {
+    case CARD_ISSUER.AMERICAN_EXPRESS:
+      return 4;
+    case CARD_ISSUER.VISA:
+    case CARD_ISSUER.MASTERCARD:
+    case CARD_ISSUER.DISCOVER:
+    case CARD_ISSUER.DINERS_CLUB:
+    case CARD_ISSUER.JCB:
+    case CARD_ISSUER.UNION_PAY:
+      return 3;
+    default:
+      return 4;
+  }
+};
+
+/**
  * Returns the appropriate security code mask based on card type.
+ * Matches iOS PaymentCardUtilityInteractor.maxSecurityCodeLength.
  * @param {string} cardNumber - The card number to detect card type from.
  * @returns {Object} Object with mask and placeholder properties.
  */
 const getSecurityCodeMask = cardNumber => {
-  if (!cardNumber) {
-    return { ...SECURITY_CODE_MASKS.STANDARD };
-  }
+  const issuer = detectCardIssuer(cardNumber);
+  const maxLength = maxSecurityCodeLength(issuer);
 
-  const cleanNumber = cardNumber.replace(/\D/g, '');
-
-  if (cleanNumber.length === 0) {
-    return { ...SECURITY_CODE_MASKS.STANDARD };
-  }
-
-  if (/^3[47]/.test(cleanNumber)) {
+  if (maxLength === 4) {
     return { ...SECURITY_CODE_MASKS.AMEX };
   }
 
   return { ...SECURITY_CODE_MASKS.STANDARD };
 };
 
-export { getSecurityCodeMask, SECURITY_CODE_MASKS };
+export { getSecurityCodeMask, maxSecurityCodeLength, SECURITY_CODE_MASKS };
 export default getSecurityCodeMask;
