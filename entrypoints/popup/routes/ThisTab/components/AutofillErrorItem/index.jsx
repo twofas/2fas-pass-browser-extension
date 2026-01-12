73a48f241aed209a696fa4d9af040ffce45a0e40
Greg Zajac <grzegorz.zajac000@gmail.com>
2026-01-12 11:28:00 +0100

    $2030 KeepItem FIX

--- entrypoints/popup/routes/ThisTab/components/AutofillErrorItem/index.jsx
+++ entrypoints/popup/routes/ThisTab/components/AutofillErrorItem/index.jsx
@@ -9,6 +9,9 @@ import generateIcon from '../../functions/serviceList/generateIcon';
 import { useState, useEffect, useRef, useCallback, memo } from 'react';
 import getItem from '@/partials/sessionStorage/getItem';
 import AutofillErrorItemData from './components/AutofillErrorItemData';
+import generateEncryptionAESKey from '@/partials/WebSocket/utils/generateEncryptionAESKey';
+import getKey from '@/partials/sessionStorage/getKey';
+import { ENCRYPTION_KEYS } from '@/constants';
 
 /**
 * Function to render an autofill error item.
@@ -26,8 +29,50 @@ function AutofillErrorItem (props) {
     }
 
     const fetchedItem = await getItem(props.deviceId, props.vaultId, props.itemId);
+
+    if (fetchedItem && props.state && typeof fetchedItem.setSifEncrypted === 'function') {
+      const sifData = [];
+
+      if (props.state.s_password) {
+        sifData.push({ s_password: props.state.s_password });
+      }
+
+      if (props.state.s_cardNumber) {
+        sifData.push({ s_cardNumber: props.state.s_cardNumber });
+      }
+
+      if (props.state.s_expirationDate) {
+        sifData.push({ s_expirationDate: props.state.s_expirationDate });
+      }
+
+      if (props.state.s_securityCode) {
+        sifData.push({ s_securityCode: props.state.s_securityCode });
+      }
+
+      if (sifData.length > 0 && props.state.hkdfSaltAB && props.state.sessionKeyForHKDF) {
+        try {
+          const encryptionItemT2Key = await generateEncryptionAESKey(
+            props.state.hkdfSaltAB,
+            ENCRYPTION_KEYS.ITEM_T2.crypto,
+            props.state.sessionKeyForHKDF,
+            true
+          );
+
+          const encryptionItemT2KeyAESRaw = await window.crypto.subtle.exportKey('raw', encryptionItemT2Key);
+          const encryptionItemT2KeyAES_B64 = ArrayBufferToBase64(encryptionItemT2KeyAESRaw);
+
+          const itemT2Key = await getKey(ENCRYPTION_KEYS.ITEM_T2.sK, { deviceId: props.deviceId, itemId: props.itemId });
+          await storage.setItem(`session:${itemT2Key}`, encryptionItemT2KeyAES_B64);
+
+          fetchedItem.setSifEncrypted(sifData);
+        } catch (e) {
+          await CatchError(e);
+        }
+      }
+    }
+
     setItem(fetchedItem);
-  }, [props?.deviceId, props?.vaultId, props?.itemId]);
+  }, [props?.deviceId, props?.vaultId, props?.itemId, props?.state]);
 
   useEffect(() => {
     getItemData();
